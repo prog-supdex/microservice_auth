4dca471160da71bc12ecb9122096b012330a4572
SupDex <symeton@gmail.com>
2021-03-17 23:44:33 +0300

    add Users::CreateService

--- app/services/users/create_service.rb
+++ app/services/users/create_service.rb
@@ -2,17 +2,22 @@ module Users
   class CreateService
     prepend BasicService
 
-    param :name
-    param :email
-    param :password
+    option :name
+    option :email
+    option :password
+    option :password_confirmation
 
     attr_reader :user
 
     def call
+      puts 'create service'
+      puts @name.inspect
+
       @user = ::User.new(
         name: @name,
         email: @email,
-        password: @password
+        password: @password,
+        password_confirmation: @password_confirmation
       )
 
       if @user.valid?
