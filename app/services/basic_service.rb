1b85d4abcbbd4e0f7a76e0567925050e2decd19c
SupDex <symeton@gmail.com>
2021-03-17 23:47:19 +0300

    fix Basic service

--- app/services/basic_service.rb
+++ app/services/basic_service.rb
@@ -1,12 +1,11 @@
 module BasicService
   module ClassMethods
     def call(**args)
-      new(**args).call
+      new(args).call
     end
   end
 
   def self.prepended(base)
-    # See https://dry-rb.org/gems/dry-initializer/3.0/skip-undefined/
     base.extend Dry::Initializer[undefined: false]
     base.extend ClassMethods
   end
