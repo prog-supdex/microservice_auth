66cc2ea55a2f5000db00614240e78e50f2ffc09b
SupDex <symeton@gmail.com>
2021-03-17 23:44:57 +0300

    add password_confirmation required

--- app/contracts/user_params_contract.rb
+++ app/contracts/user_params_contract.rb
@@ -3,5 +3,6 @@ class UserParamsContract < Dry::Validation::Contract
     required(:name).value(:string)
     required(:email).value(:string)
     required(:password).value(:string)
+    required(:password_confirmation).value(:string)
   end
 end
