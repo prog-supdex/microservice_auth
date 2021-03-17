bceb28e72653389e9e21dfc0266ac36a952478ce
SupDex <symeton@gmail.com>
2021-03-17 22:44:33 +0300

    remove load application_job from application_loader

--- config/application_loader.rb
+++ config/application_loader.rb
@@ -29,7 +29,6 @@ module ApplicationLoader
     require_dir 'app/helpers'
     require_file 'config/application'
     require_file 'app/services/basic_service'
-    require_file 'app/jobs/application_job'
 
     require_dir 'app'
   end
