7a88d017a7a69c1a2b64cab15962aaee61ccb2da
SupDex <symeton@gmail.com>
2021-03-17 23:46:05 +0300

    add validatef for format name in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,6 @@ class User < Sequel::Model
     validates_presence :email,
                        message: I18n.t(:blank, scope: 'model.errors.user.email')
 
-    validates_format NAME_FORMAT, :name, message: I18n.t(:blank, scope: 'model.errors.user.incorrect.name')
+    validates_format NAME_FORMAT, :name, message: I18n.t(:format, scope: 'model.errors.user.name')
   end
 end
