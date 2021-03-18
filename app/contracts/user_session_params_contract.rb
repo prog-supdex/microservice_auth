87832b9bfeab876f02889bd786d11bb23a678007
SupDex <symeton@gmail.com>
2021-03-18 17:02:10 +0300

    checking for filled params in UserSessionParamsContract

--- app/contracts/user_session_params_contract.rb
+++ app/contracts/user_session_params_contract.rb
@@ -1,6 +1,6 @@
 class UserSessionParamsContract < Dry::Validation::Contract
   params do
-    required(:email).value(:string)
-    required(:password).value(:string)
+    required(:email).filled(:string)
+    required(:password).filled(:string)
   end
 end
