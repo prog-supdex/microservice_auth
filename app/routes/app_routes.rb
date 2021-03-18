bf76a6f40c17c3a581ae10e1693fd7f89c9ff03b
SupDex <symeton@gmail.com>
2021-03-18 20:23:30 +0300

    change name routes

--- app/routes/app_routes.rb
+++ app/routes/app_routes.rb
@@ -2,8 +2,8 @@ class AppRoutes < Application
   use Rack::CommonLogger, Logger.new($stdout)
 
   route do |r|
-    r.is true,          proc { r.run AuthRoutes }
-    r.on 'users/v1',    proc { r.run UserRoutes }
-    r.on 'sessions/v1', proc { r.run UserSessionRoutes }
+    r.on 'v1/auth',     proc { r.run AuthRoutes }
+    r.on 'v1/users',    proc { r.run UserRoutes }
+    r.on 'v1/sessions', proc { r.run UserSessionRoutes }
   end
 end
