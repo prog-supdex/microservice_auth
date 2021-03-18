b8c4efe83c62dade719f2a0363a03ab907fbf684
SupDex <symeton@gmail.com>
2021-03-18 07:36:50 +0300

    final create sessions/v1 in AuthRoutes

--- app/routes/auth_routes.rb
+++ app/routes/auth_routes.rb
@@ -5,7 +5,21 @@ class AuthRoutes < Application
     r.post do
       r.on 'sessions/v1' do
         r.is do
+          params = JSON.parse(r.body.read)
+          session_params = r.validate_with!(validation: UserSessionParamsContract, params: params)
+          result = UserSessions::CreateService.call(**session_params.to_h)
 
+          if result.success?
+            token = JwtEncoder.encode(uuid: result.session.uuid)
+            meta = { token: token }
+
+            response.status = :created
+
+            { meta: meta }
+          else
+            response.status = :unauthorized
+            error_response(result.session || result.errors)
+          end
         end
       end
 
