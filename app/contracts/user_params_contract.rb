d6abbdf43b10e6f648d17489d0c0a433da7d0680
SupDex <symeton@gmail.com>
2021-03-21 16:23:37 +0300

    remove password_confirmation required attribute from UserParamsCOntract

--- app/contracts/user_params_contract.rb
+++ app/contracts/user_params_contract.rb
@@ -3,6 +3,5 @@ class UserParamsContract < Dry::Validation::Contract
     required(:name).value(:string)
     required(:email).value(:string)
     required(:password).value(:string)
-    required(:password_confirmation).value(:string)
   end
 end
